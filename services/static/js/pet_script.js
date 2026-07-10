b9ac84c6222343383975b7d0c00d0c1a5038c980
LiuLifeng <2721858715@qq.com>
2026-07-11 05:06:55 +0800

    fix: restore mousemove else branch and remove all [DRAG DEBUG] console logs for a clean production state

--- services/static/js/pet_script.js
+++ services/static/js/pet_script.js
@@ -134,12 +134,9 @@ class RumiaPet {
         if (rumiaIPC) {
             let isDragging = false;
             let startX = 0, startY = 0;
-            let lastInteractive = null;
-            let moveCount = 0;
 
             // mousedown handler
             this.img.addEventListener('mousedown', (e) => {
-                console.log(`[DRAG DEBUG] mousedown on pet. Button: ${e.button}, isSleeping: ${this.isSleeping}`);
                 if (this.isSleeping) {
                     this.wakeUp(false);
                 }
@@ -147,7 +144,6 @@ class RumiaPet {
                     isDragging = true;
                     startX = e.screenX;
                     startY = e.screenY;
-                    console.log(`[DRAG DEBUG] Drag started. initial screenX/Y: (${startX}, ${startY})`);
                     rumiaIPC.sendSetIgnoreMouseEvents(false);
                     this.img.style.cursor = 'grabbing';
                 }
@@ -164,8 +160,8 @@ class RumiaPet {
                     const deltaY = e.screenY - startY;
                     startX = e.screenX;
                     startY = e.screenY;
-                    console.log(`[DRAG DEBUG] Dragging mousemove. Delta: (${deltaX}, ${deltaY})`);
                     rumiaIPC.sendWindowDrag(deltaX, deltaY);
+                } else {
                     let isInteractive = false;
                     const el = e.target;
                     
