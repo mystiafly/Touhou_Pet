5ba9937e202a0e59222b52b428fd842b44f4024e
LiuLifeng <2721858715@qq.com>
2026-07-29 03:46:56 +0800

    chore: remove old scripts from root directory properly

--- test_upload.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-url = "http://127.0.0.1:5000/api/characters/import"
-files = {'file': open('wriggle_test.zip', 'rb')}
-data = {'char_id': 'wriggle_test'}
-
-response = requests.post(url, files=files, data=data)
-print(response.status_code)
-print(response.json())
